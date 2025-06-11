88c206658487372e78cb42bde32f193b42840f09
Saurabh9122-tech <ss061959@gmail.com>
2025-06-11 15:27:41 +0530

    fix the bug

--- server.js
+++ server.js
@@ -1,9 +1,9 @@
+require('dotenv').config();
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
 const path = require('path');
 const authRoutes = require('./routes/auth');
-require('dotenv').config();
 const app = express();
 const PORT = 5000;
 app.use(cors());
